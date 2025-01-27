37512140bfa67655d4117369be28ac395a688de5
Michael Owuor <michael.owuor@gmail.com>
2025-01-27 09:26:09 +0300

    refactor: clean up imports and update package dependencies

--- src/utilities/generatePreviewPath.ts
+++ src/utilities/generatePreviewPath.ts
@@ -1,36 +1,37 @@
-import { PayloadRequest, CollectionSlug } from 'payload'
+import { PayloadRequest, CollectionSlug } from "payload";
+
+import { env } from "@/utilities/env";
 
 const collectionPrefixMap: Partial<Record<CollectionSlug, string>> = {
-  posts: '/posts',
-  pages: '',
-}
+	posts: "/posts",
+	pages: "",
+};
 
 type Props = {
-  collection: keyof typeof collectionPrefixMap
-  slug: string
-  req: PayloadRequest
-}
+	collection: keyof typeof collectionPrefixMap;
+	slug: string;
+	req: PayloadRequest;
+};
 
 export const generatePreviewPath = ({ collection, slug, req }: Props) => {
-  const path = `${collectionPrefixMap[collection]}/${slug}`
+	const path = `${collectionPrefixMap[collection]}/${slug}`;
 
-  const params = {
-    slug,
-    collection,
-    path,
-  }
+	const params = {
+		slug,
+		collection,
+		path,
+	};
 
-  const encodedParams = new URLSearchParams()
+	const encodedParams = new URLSearchParams();
 
-  Object.entries(params).forEach(([key, value]) => {
-    encodedParams.append(key, value)
-  })
+	Object.entries(params).forEach(([key, value]) => {
+		encodedParams.append(key, value);
+	});
 
-  const isProduction =
-    process.env.NODE_ENV === 'production' || Boolean(process.env.VERCEL_PROJECT_PRODUCTION_URL)
-  const protocol = isProduction ? 'https:' : req.protocol
+	const isProduction = process.env.NODE_ENV === "production" || Boolean(process.env.VERCEL_PROJECT_PRODUCTION_URL);
+	const protocol = isProduction ? "https:" : req.protocol;
 
-  const url = `${protocol}//${req.host}/next/preview?${encodedParams.toString()}`
+	const url = `${protocol}//${req.host}/next/preview?${encodedParams.toString()}`;
 
-  return url
-}
+	return url;
+};
